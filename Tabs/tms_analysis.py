eb604dbbc6e1668184cf311541d92a095e871b2f
Dayanand Sah <daya123ab@gmail.com>
2026-03-07 09:43:22 +0545

    Update tms_analysis.py

--- Tabs/tms_analysis.py
+++ Tabs/tms_analysis.py
@@ -7,9 +7,11 @@ try:
     from Tabs.SubTabs import Stock_Hold
     from Tabs.SubTabs import Visual
     from Tabs.SubTabs import Whale
+    from Tabs.SubTabs import TMS_Holdings
     importlib.reload(Stock_Hold)
     importlib.reload(Visual)
     importlib.reload(Whale)
+    importlib.reload(TMS_Holdings)
       
 except ImportError:
     st.error("❌ Failed to load SubTabs. Check your __init__.py files.")
@@ -24,10 +26,11 @@ def run():
 
     
     # Define SubTabs
-    tab1, tab2, tab3 = st.tabs([
+    tab1, tab2, tab3, tab4 = st.tabs([
         "🗃️ Ledger Matrix", 
         "👁️ Visual Flow", 
-        "🐳 Whale Tracker"
+        "🐳 Whale Tracker",
+        "🏢 TMS Holdings"
     ])
     
     with tab1:
@@ -47,6 +50,12 @@ def run():
     with tab3: # <--- INJECTING WHALE.PY HERE
         try:
             Whale.run()
+        except Exception as e:
+            st.error(f"🛑 CRITICAL SYSTEM FAILURE: {str(e)}")
+            st.exception(e)
+    with tab4: 
+        try:
+            TMS_Holdings.run()
         except Exception as e:
             st.error(f"🛑 CRITICAL SYSTEM FAILURE: {str(e)}")
             st.exception(e)
